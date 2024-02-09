8f2068697abd00c63bd1c74589f96b3ae9b7f498
mejia201 <bryan.mejia@catolica.edu.sv>
2024-02-08 18:11:24 -0600

    Agregando modelo pedido

--- src/main/java/ModelsDao/PedidoDao.java
+++ src/main/java/ModelsDao/PedidoDao.java
@@ -1,6 +1,16 @@
 package ModelsDao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 
 public class PedidoDao {
+    
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    int r;
+    
     
 }
