494b75df00d1b45a0ebd058fa4db69c827e63ebd
mejia201 <bryan.mejia@catolica.edu.sv>
2024-02-12 12:12:23 -0600

    Agregando pedidos

--- src/main/java/Controllers/PedidoController.java
+++ src/main/java/Controllers/PedidoController.java
@@ -1,7 +1,13 @@
 package Controllers;
 
+import Models.Pedido;
+import ModelsDao.PedidoDao;
+import Utils.General;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,61 +17,95 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "PedidoController", urlPatterns = {"/Pedido"})
 public class PedidoController extends HttpServlet {
-
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet PedidoController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet PedidoController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+    
+    
+    PedidoDao pedidoService;
+    
+    public PedidoController(){
+        
+        pedidoService = new PedidoDao();
+        
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+ 
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+         
+        List<Pedido> pedidos = pedidoService.ListPedidos();
+        
+       General.sendAsJson(response, General.ObjectToJson(pedidos));
+
+        return;
+        
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        
+        String nombre = request.getParameter("cliente");
+        String fechaString = request.getParameter("fecha");
+        String total = request.getParameter("total");
+        String estado = request.getParameter("estado");
+        
+        Pedido pedido = new Pedido();
+       
+        pedido.setId_cliente(Integer.parseInt(nombre));
+        
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date fecha = null;
+        try {
+            fecha = dateFormat.parse(fechaString);
+        } catch (ParseException e) {
+            
+            e.printStackTrace(); 
+        }
+
+        
+        pedido.setFecha(fecha);
+
+  
+        pedido.setTotal(Float.parseFloat(total));
+        pedido.setEstado(Integer.parseInt(estado));
+        
+        if(pedidoService.AddPedido(pedido)){
+            General.sendAsJson(response, General.ObjectToJson(pedido));
+            return;
+        }
+        
+        General.sendAsJson(response, "[]");
+        return;
+       
+    }
+    
+    
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        String id = req.getParameter("id");
+        
+        if(id=="" || id == null){
+            General.sendAsJson(resp, "[{}]");
+            return;
+        }
+        
+        if(pedidoService.DeletePedido(Integer.parseInt(id))){
+            General.sendAsJson(resp, "[{\"id\": " + id + "}]");
+            return;
+        }else{
+            General.sendAsJson(resp, "[{}]");
+            return;
+        }
+        
+        
+    
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+
+  
 
 }
--- src/main/java/ModelsDao/PedidoDao.java
+++ src/main/java/ModelsDao/PedidoDao.java
@@ -1,12 +1,107 @@
 package ModelsDao;
 
+import Models.Pedido;
+import Persistencia.MySQLConnection;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PedidoDao {
     
+        public List<Pedido> ListPedidos() {
+        List<Pedido> pedidos = new ArrayList<>();
+
+        try {
+            MySQLConnection connection = MySQLConnection.getInstance();
+            Connection conn = connection.getConnection();
+
+            String query = "SELECT * FROM pedidos;";
+
+            PreparedStatement statement = conn.prepareStatement(query);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                Pedido pedido = new Pedido();
+
+                pedido.setId(resultSet.getInt("id"));
+                pedido.setId_cliente(resultSet.getInt("id_cliente"));
+                pedido.setFecha(resultSet.getDate("fecha"));
+                pedido.setTotal(resultSet.getFloat("total"));
+                pedido.setEstado(resultSet.getInt("estado"));
+
+                pedidos.add(pedido);
+            }
+
+            resultSet.close();
+            statement.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return pedidos;   
+    }
+
+    
+    public boolean AddPedido(Pedido pedido){
+        boolean IsAdded = false;
+    
+        try {
+            
+            MySQLConnection connection = MySQLConnection.getInstance();
+            Connection conn = connection.getConnection();
+            
+            String query = "INSERT INTO `pedidos` (`id`, `id_cliente`, `fecha`, `total`, `estado`) VALUES (NULL, ?, ?, ?, ?);";
+            
+            PreparedStatement statement = conn.prepareStatement(query);
+            
+            statement.setInt(1, pedido.getId_cliente());
+            statement.setDate(2, (Date) pedido.getFecha());
+            statement.setFloat(3, pedido.getTotal());
+            statement.setInt(4, pedido.getEstado());
+            
+            int rowsAffected = statement.executeUpdate();
+            
+            if(rowsAffected > 0){
+                IsAdded = true;
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return IsAdded;
+    }
+    
+    public boolean DeletePedido(int id){
+        boolean isDeleted = false;
+        
+        try{
+              MySQLConnection connection = MySQLConnection.getInstance();
+            Connection conn = connection.getConnection();
+            
+            String query = "DELETE FROM `pedidos` WHERE id = ?;";
+            
+            PreparedStatement statement = conn.prepareStatement(query);
+            
+            statement.setInt(1, id);
+            
+            int rowsAffected = statement.executeUpdate();
+            
+            if(rowsAffected > 0){
+                isDeleted = true;
+            }
+        }catch(Exception error){
+            error.printStackTrace();
+        }
+        
+        return isDeleted;
+    }
 
     
 }
